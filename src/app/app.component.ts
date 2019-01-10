6b1db50326ee3d21495a835051c29ab4e071c495
ShihabulIslamMahir <shihab.mahi1995@gmail.com>
2019-01-10 11:21:07 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'amte-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-material-theming-task';
+  theme = 'theme-one';
+
+  constructor() {
+    if (localStorage.getItem('themeSelection') === null) {
+      localStorage.setItem('themeSelection', 'theme-one');
+    } else {
+      this.theme = localStorage.getItem('themeSelection');
+    }
+  }
+
+  themeChanging(theme) {
+    this.theme = theme;
+    localStorage.setItem('themeSelection', this.theme);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './../../material-module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatIconModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
